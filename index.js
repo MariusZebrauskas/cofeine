5faed3bd51711261915c37d15fac107f3c1ca4a9
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2024-11-17 15:43:15 +0200

    updated packages

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require("express");
 const axios = require("axios");
 const schedule = require("node-schedule");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
 
+app.use(express.json());
+app.use(morgan("common"));
+app.use(cors());
+app.use(helmet());
 // Array of websites to call
-const websites = ["https://porfolio-new-dabd3275f18f.herokuapp.com/ping"];
+const websites = [
+  "https://porfolio-new-dabd3275f18f.herokuapp.com/ping",
+  "http://localhost:8800/ping",
+];
 
 // Function to call a website
 async function callWebsite(url) {
@@ -21,7 +34,7 @@ async function callWebsite(url) {
 }
 
 // Schedule the job to call each website every 1 minutes
-const job = schedule.scheduleJob("*/01 * * * *", function () {
+const job = schedule.scheduleJob("*/10 * * * * *", function () {
   console.log("Scheduled job executed at:", new Date());
   websites.forEach((url) => callWebsite(url));
 });
