9d3fb285ca73a8716c170425ecfd9a2c344744d7
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2024-11-17 16:29:07 +0200

    web: node index.js

--- index.js
+++ index.js
@@ -8,21 +8,20 @@ const PORT = process.env.PORT || 3000;
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
-const path = require("path");
 
 app.use(express.json());
 app.use(morgan("common"));
 app.use(cors());
 app.use(helmet());
+
 // Array of websites to call
 const websites = [
   "https://porfolio-new-dabd3275f18f.herokuapp.com/ping",
-  "http://localhost:8800/ping",
+  "http://localhost:8800/ping", // Ensure this is only used in development or when the server is accessible
 ];
 
 // Function to call a website
 async function callWebsite(url) {
-  //please send schedulerUrl === PORT
   try {
     const response = await axios.post(url, {
       schedulerUrl: "https://cofeine-af492e410cef.herokuapp.com/ping",
@@ -33,15 +32,24 @@ async function callWebsite(url) {
   }
 }
 
-// Schedule the job to call each website every 1 minutes
-const job = schedule.scheduleJob("*/10 * * * * *", function () {
+// Schedule the job to call each website every 10 seconds
+const job = schedule.scheduleJob("*/10 * * * * *", async function () {
   console.log("Scheduled job executed at:", new Date());
-  websites.forEach((url) => callWebsite(url));
+  for (const url of websites) {
+    await callWebsite(url);
+  }
+  // Attempt to reset or call another endpoint after all websites have been called
+  try {
+    await axios.get("https://cofeine-af492e410cef.herokuapp.com/ping");
+    console.log("Successfully reset my clock");
+  } catch (error) {
+    console.error("Error resetting clock:", error);
+  }
 });
 
+// Corrected GET handler for '/ping' route
 app.get("/ping", (req, res) => {
-  res.send("pinged");
-  res.status(200).send("Pong");
+  res.status(200).send("Hello World");
 });
 
 app.listen(PORT, () => {
